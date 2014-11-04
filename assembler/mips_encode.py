94090d7635b0b643385a226841c8b96268bdae92
denil1111 <zyh.work@gmail.com>
2014-11-04 09:25:34 +0800

    debug first step

--- assembler/mips_encode.py
+++ assembler/mips_encode.py
@@ -4,11 +4,19 @@ def encode(input_list):
 	mode=''
 	instruction_list=[]
 	variable_list={}
+	label_list={}
 	temp=None
 	max_address=0
 	data_set=[]
 	for instruction_input in input_list:
-		instruction_input.strip(' ')
+		instruction_input=instruction_input.strip()
+		print("======NOW instruction======")
+		print (instruction_input)
+		print("===========================")
+		if (instruction_input==""):
+			continue
+		if (instruction_input[0]=="#"):
+			continue
 		if (instruction_input=='.data'):
 			mode='data'
 			continue
@@ -17,50 +25,83 @@ def encode(input_list):
 			continue
 		if (mode=='data'):
 			if (temp):
+				print ("have temp")
+				print (temp.content)
 				if (temp.content==''):
-					temp.add_content(instruction_input)
-					continue
-			temp=UserInstruction(instruction_input)
+					temp.add_instruction(instruction_input)
+			else:
+				temp=UserInstruction(instruction_input)
+			print ("label:",temp.label)
 			if (temp.content!=''):
-				variable_list[temp.label]=max_address
+				print('now content:'+temp.content)
+				if (temp.label):
+					for each_label in temp.label:
+					# print("now label:"+temp.label)
+						try:
+							test=variable_list[each_label]
+							raise ValueError('redefine label:'+each_label)
+						except:
+							variable_list[each_label]=max_address
 				newdatas=initdata(temp.content)
 				data_set+=newdatas
 				max_address+=len(newdatas)
+				temp=None
 		if (mode=='text'):
 			# print (instruction_input)
 			if (temp):
+				print ("have temp")
 				if (temp.content==''):
-					temp.add_content(instruction_input)
-					instruction_list.append(temp)
-					continue
-			temp=UserInstruction(instruction_input)
+					temp.add_instruction(instruction_input)
+			else:
+				print("no temp")
+				temp=UserInstruction(instruction_input)
 			if (temp.content!=''):
+				print("add!")
 				instruction_list.append(temp)
+				temp=None
 	real_instruction_list=[]
+	print ("====first step finished====")
+	print ("====var list=====")
+	for each_label in variable_list:
+		print (each_label)
 	for instruction in instruction_list:
+		print("=====now instruction=====",instruction.content,"|label:",instruction.label)
 		new_real_list=pesudo_encode(instruction,variable_list)
-		print('=====')
+		print('===new===')
 		for show in new_real_list:
-			print(show.label,'[]',show.content,'[]',show.note,'[]')
-		print('=====')
+			print('label:',show.label,'content:',show.content,'note,',show.note)
+		print('=========')
 		real_instruction_list+=new_real_list
 	label_list={}
+	print("===real instruction start===")
 	for index,instruction in enumerate(real_instruction_list):
+		print("=====now instruction=====",instruction.content,"|label:",instruction.label)
 		if (instruction.label):
-			try:
-				test=label_list[instruction.label]
-				raise ValueError('redefine label:'+instruction.label)
-			except:
-				label_list[instruction.label]=index
+			print ("handling labellist",instruction.label)
+			for each_label in instruction.label:
+				try:
+					test=label_list[each_label]
+					raise ValueError('redefine label:'+instruction.label)
+				except:
+					label_list[each_label]=index
+	print ("======label list======")
+	for each_label in label_list:
+		print (each_label)
+	# print (label_list["clear"])
+	print ("======begin encode======")
 	for index,instruction in enumerate(real_instruction_list):
+		print ("======now ins======"+ instruction.content)
 		instruction.code=single_encode(instruction.content,label_list,index)
 	for instruction in real_instruction_list:
+		# pass
+		print (instruction.content)
 		print (instruction.code)
+print("========program start=======")
 ins_file=open('instruction.txt')
 input_list=[]
 for line in ins_file:
 	input_list.append(line[:-1])
-	print (line[:-1])
+	# print (line[:-1])
 encode(input_list)
 # try:
 # 	a=single_encode(input_instruction)
--- assembler/pesudo.py
+++ assembler/pesudo.py
@@ -3,10 +3,12 @@ from initial import get_code
 pesudo_list=['la','li']
 def encode_pesudo_la(origin_instruction,var_list):
 	# print (var_list)
+	# print ("ins:",origin_instruction)
 	la_match=re.compile('(?P<rs>.*?),(?P<variable>.*)').match(origin_instruction)
 	if (not la_match):
 		raise ValueError('Instruction syntax error')
 	variable=la_match.group('variable')
+	# print ("var:",variable)
 	rs=la_match.group('rs')
 	real_list=[]
 	try:
@@ -16,7 +18,7 @@ def encode_pesudo_la(origin_instruction,var_list):
 		high_code=address_code[:16]
 		low_code=address_code[16:]
 		real_list+=['lui '+rs+','+'0b'+high_code]
-		real_list+=['or '+rs+','+rs+','+low_code]
+		real_list+=['ori '+rs+','+rs+','+'0b'+low_code]
 	except:
 		raise ValueError('no variable:'+variable)
 	return real_list
@@ -33,7 +35,7 @@ def encode_pesudo_li(origin_instruction,var_list):
 		high_code=immediate_code[:16]
 		low_code=immediate[16:]
 		real_list+=['lui'+rs+','+'0b'+high_code]
-		real_list+=['ori'+rs+','+rs+','+low_code]
+		real_list+=['ori'+rs+','+rs+','+'0b'+low_code]
 	except SyntaxError:
 		raise ValueError('Error expression:'+ immediate)
 	return real_list
